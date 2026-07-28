95b8121cc5189285c734a7c0ac296a493bd22785
Kawade Dipali Anil <dipalikawade08@gmail.com>
2026-07-28 22:57:46 +0530

    first commit

--- Coding Practice/as2Q6.py
+++
@@ -1,7 +0,0 @@
-print("----- Membership Operators -----")
-
-languages = ["Python", "C", "C++", "JavaScript"]
-
-print("Python exists :", "Python" in languages)
-print("Java does not exist :", "Java" not in languages)
-print("C++ exists :", "C++" in languages)
